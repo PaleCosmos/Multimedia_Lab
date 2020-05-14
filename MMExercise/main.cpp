c60cfffc2de06a10344dbf23a0058fede3bc6e25
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2020-05-14 14:57:02 +0900

    result

--- MMExercise/main.cpp
+++ MMExercise/main.cpp
@@ -2,21 +2,22 @@
 
 using namespace lab;
 
-void showImage(std::string src) {
-	cv::imshow("Origin", cv::imread(src.c_str(), cv::IMREAD_COLOR));
+void showImage(std::string src, std::string name) {
+	cv::imshow(name, cv::imread(src.c_str(), cv::IMREAD_COLOR));
 }
 
 void show() {
 	// 비교
-	showImage("..\\image\\lena_noise.png");
+	showImage("..\\image\\lena_noise.png", "lena_noise");
 	lab3_3_1();
 	lab3_3_2();
 }
 
 int main() {
-	//show();
-	//showImage("..\\image\\moon_blur.png");
-	//lab3_3_3();
+	std::string imageName = "..\\image\\gg.jpg";
+	std::string imageName2 = "..\\image\\moon.jpg";
+	showImage(imageName, "chroma");
+	showImage(imageName2, "mooon");
 	lab5_1();
 
 	return 0;
